f11764e12f662ff5a2f0e6958e39a42c7f57a1d8
Hannes Georg <hannes.georg@googlemail.com>
2012-06-20 23:11:44 +0200

    Allow RangeSet to be constructed from a start and end

--- src/kraken/range_set.h
+++ src/kraken/range_set.h
@@ -7,11 +7,24 @@ namespace Kraken {
 template< typename K >
 class RangeSet : public RangeMap<K, bool>{
   public:
+
 void add( K mm , std::function<bool(bool,bool)> merger = overwrite<bool>() ){
   set( mm, true , merger );
 }
+
 void add( K min, K max , std::function<bool(bool,bool)> merger = overwrite<bool>() ){
   set( min, max , true , merger );
+}
+
+void add( const RangeSet<K> &se, std::function<bool(bool,bool)> merger = overwrite<bool>() ){
+  set( se , true , merger );
+}
+
+RangeSet() : RangeMap<K,bool>() {
+}
+
+RangeSet( K min, K max ) : RangeMap<K,bool>() {
+  add( min, max );
 }
 
 RangeSet<K> & operator+=( const RangeSet<K> &s ){
