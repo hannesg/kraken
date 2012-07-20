ddef00e7f3a04bcc86f1960fc9c275150e1cde71
Hannes Georg <hannes.georg@googlemail.com>
2012-07-20 12:56:45 +0200

    Added even another Node::Map test

--- tests/test_node_map.cpp
+++ tests/test_node_map.cpp
@@ -34,8 +34,24 @@ void test_node_map2(){
 	}
 }
 
+void test_node_map3(){
+	Kraken::Node::Pool pool;
+	Kraken::Node::Map* node = pool.make<Kraken::Node::Map>();
+	mock_string s = { };
+
+	node->set( 10, 20, pool.make<Kraken::Node::Terminal>(1) );
+	Kraken::Node::Result result = node->traverse(&s);
+
+	if( result.isError() ){
+		std::cout << ".";
+	}else{
+		std::cout << "Result wasn't error.";
+	}
+}
+
 int main(int argc, char** argv){
 	test_node_map1();
 	test_node_map2();
+	test_node_map3();
 	return 0;
 }
